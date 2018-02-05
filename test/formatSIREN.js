24e2d24d21b1f5f5b9cedf0393fe5460a7e33c6b
Achille Urbain <achille.urbain@gmail.com>
2018-02-05 23:44:51 +0100

    Add formatSIRET, formatSIREN, formatVAT methods + tests

---
+++ test/formatSIREN.js
@@ -0,0 +1,11 @@
+const lib = require('../index');
+
+[0, 1, true, false, {}, [], '81345471', '8134547171', 'FR42813454717', '81345471700014'].forEach((value) => {
+  test(`formatSIREN bad value ${JSON.stringify(value)}`, () => {
+    expect(() => lib.formatSIREN(value)).toThrow('Not a valid SIREN');
+  });
+});
+
+test(`format SIREN good values`, () => {
+  expect(['813454717', '803417153'].map(lib.formatSIREN)).toEqual(['813 454 717', '803 417 153']);
+});
