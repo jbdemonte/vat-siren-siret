83fc5e78cb1d16cbd51fd896748d3f121eef899e
Achille Urbain <achille.urbain@gmail.com>
2018-02-28 10:37:37 +0100

    Fix tests

--- test/formatSIREN.js
+++ test/formatSIREN.js
@@ -1,9 +1,8 @@
 const lib = require('../index');
 
-[0, 1, true, false, {}, [], '81345471', '8134547171', 'FR42813454717', '81345471700014'].forEach((value) => {
-  test(`formatSIREN bad value ${JSON.stringify(value)}`, () => {
-    expect(() => lib.formatSIREN(value)).toBe(value);
-  });
+test(`test SIREN bad values`, () => {
+  const badValues = [0, 1, true, false, {}, [], '81345471', '8134547171', 'FR42813454717', '81345471700014'];
+  expect(badValues.map(lib.formatSIREN)).toEqual(badValues);
 });
 
 test(`format SIREN good values`, () => {
